b90e3376278d0a3d0cb97c871a19e2bf77ee9830
JosephPenaQuino <joseph.pena@utec.edu.pe>
2019-06-19 16:09:06 -0500

    Add main files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#ifndef CATCH_CONFIG_MAIN
+#include <iostream>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+#endif
---
+++ main_test.cpp
@@ -0,0 +1,17 @@
+#include "catch.hpp"
+#include "main.h"
+#include <iostream>
+
+SCENARIO("")
+{
+    GIVEN("")
+    {
+        WHEN("")
+        {
+            THEN("")
+            {
+                REQUIRE(false);
+            }
+        }
+    }
+}
