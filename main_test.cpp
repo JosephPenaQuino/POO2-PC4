ffeea8aa7a0c1a6e5f195a9b980636fe2e1c4013
JosephPenaQuino <joseph.pena@utec.edu.pe>
2019-06-19 16:20:58 -0500

    More TDD

--- main_test.cpp
+++ main_test.cpp
@@ -4,14 +4,44 @@
 
 SCENARIO("Matrix multiplication Comparison between Concurrent and Non-concurrent")
 {
-    GIVEN("Concurrent Algorithm")
+    GIVEN("Non-Concurrent Algorithm")
     {
-        WHEN("")
+        NMatrix my_matrix_1;
+        NMatrix my_matrix_2;
+        NMatrix my_matrix_result;
+        WHEN("Setting size of each matrix")
         {
-            THEN("")
+            // TODO: Get size of each matrix
+
+            // TODO: Set size on both matrices
+
+            THEN("Multiply both matrices")
             {
-                REQUIRE(false);
+                my_matrix_result = my_matrix_1 * my_matrix_2;
+                REQUIRE(my_matrix_result.print_values());
             }
         }
     }
+    GIVEN("Concurrent Algorithm")
+    {
+
+        WHEN("Setting size of each matrix")
+        {
+            CMatrix my_matrix_1;
+            CMatrix my_matrix_2;
+            CMatrix my_matrix_result;
+
+            // TODO: Get size of each matrix
+
+            // TODO: Set size on both matrices
+
+            THEN("Multiply both matrices")
+            {
+
+                my_matrix_result = my_matrix_1 * my_matrix_2;
+                REQUIRE(my_matrix_result.print_values());
+            }
+        }
+    }
+
 }
